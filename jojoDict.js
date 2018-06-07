2c394051c7c8de90c4374e46e85857d66bdf6ba1
YiningHua <izaya1997@163.com>
2018-06-07 15:38:05 +0800

    find()
    replaced  find()

--- jojoDict.js
+++ jojoDict.js
@@ -4,40 +4,48 @@ fetch('data.json').then(function(response) {
   if(response.ok) {
     response.json().then(function(json) {
       data = json;
-      initialize();
     });
   } else {
     console.log('Network request for data.json failed with response ' + response.status + ': ' + response.statusText);
   }
 });
 
-var language = document.querySelector('#language');
-var searchTerm = document.getElementById('searchTerm');
+    var language = document.querySelector('#language');
+    var searchTerm = document.getElementById('searchTerm');
 
 function returnValue(){//get language selection
     if (language.value==="CN->EN"){
-        
-        var index = data.find(x => x.CN === searchTerm.value);
-        
-        if(typeof(index)!="undefined"){
-            var ENresult = index.EN;
-            document.getElementById("output").innerHTML = ENresult;
+        var ENresult;
+        var found = false;
+        for(var i = 0; i < data.length; i++){
+            if (data[i].CN === searchTerm.value){
+                ENresult=data[i].EN;
+                found = true;
+            }
+        }
+        if (found == true){
+        document.getElementById("output").innerHTML = ENresult;
         }
         else{
-            document.getElementById("output").innerHTML = "Target word not found.";
-            }
+        document.getElementById("output").innerHTML = "Target word not found.";
+        }
     }
     else if(language.value==="EN->CN"){
         
-        var index = data.find(x => x.EN === searchTerm.value);        
-
-        if(typeof(index)!="undefined"){
-            var CNresult = index.CN
-            document.getElementById("output").innerHTML = CNresult;
+        var CNresult;
+        var found = false;
+        for(var i = 0; i < data.length; i++){
+            if (data[i].EN === searchTerm.value){
+                CNresult=data[i].CN;
+                found = true;
+            }
+        }
+        if (found==true){
+        document.getElementById("output").innerHTML = CNresult;
         }
         else{
-            document.getElementById("output").innerHTML = "Target word not found.";
-            }
+        document.getElementById("output").innerHTML = "Target word not found.";
+        }
     }
 }
 
