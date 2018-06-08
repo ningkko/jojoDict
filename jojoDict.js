d10375de4ca5f2cdf6ffb885ddbdc79765d53831
YiningHua <izaya1997@163.com>
2018-06-08 23:49:46 +0800

    AUTOINPUT
    dataupgrade
    autoinput

--- jojoDict.js
+++ jojoDict.js
@@ -11,10 +11,11 @@ fetch('data.json').then(function(response) {
 });
 
     var language = document.querySelector('#language');
-    var searchTerm = document.getElementById('searchTerm');
+    var searchTerm = document.getElementById('input');
 
 function returnValue(){//get language selection
     if (language.value==="CN->EN"){
+        var array=['a','aa']
         var ENresult;
         var found = false;
         for(var i = 0; i < data.length; i++){
@@ -61,3 +62,148 @@ function returnValue(){//get language selection
     }
 }
 
+// 数组去重
+Array.prototype.unique = function(){
+	this.sort();
+	var res = [];
+	var json = {};
+	for (var i = 0; i < this.length; i++) {
+		if(!json[this[i]]){
+			res.push(this[i]);
+			json[this[i]] = 1;
+		}
+	}
+	return res;
+}
+
+// 对样式操作
+var setClass = {
+	hasClass: function(elements,cName){	// 判断是否含有某个class
+		if(elements.className.match(new RegExp( "(\\s|^)" + cName + "(\\s|$)") ))
+			return true;
+		else
+			return false;
+	},
+	addClass: function(elements,cName){	// 添加class
+		if( !this.hasClass( elements,cName ) ){ 
+			elements.className += " " + cName; 
+		};
+	},
+	removeClass: function(elements,cName){	// 移除某个class
+		if( this.hasClass( elements,cName ) ){ 
+			elements.className = elements.className.replace( new RegExp( "(\\s|^)" + cName + "(\\s|$)" )," " ); 
+		}
+	}
+}
+
+var Bind = function(This){
+	return function(){
+		This.init();
+	}
+}
+
+function AutoComplete(input,auto,arr) {
+	this.obj = document.getElementById(input);
+	this.autoObj = document.getElementById(auto);
+	this.search_value = "";	//当前的搜索输入值
+	this.index = -1;		//当前选中的DIV的索引
+	this.value_arr = arr;	//数据库中供检索的值 不包含重复值
+}
+
+AutoComplete.prototype = {
+    init: function(){
+		var This = this;
+		setClass.removeClass(This.autoObj,"hidden");
+		this.autoObj.style.left = this.obj.offsetLeft + "px";
+		this.autoObj.style.top = this.obj.offsetTop + this.obj.offsetHeight + "px";
+	},
+
+    deleteDIV: function(){
+		while(this.autoObj.hasChildNodes()){
+			this.autoObj.removeChild(this.autoObj.firstChild);
+		}
+		setClass.addClass(this.autoObj,"hidden");
+	},
+	
+    autoOnmouseover: function(index){
+		if(index != this.index){
+			setClass.addClass(this.autoObj.children[index],"on");
+			setClass.removeClass(this.autoObj.children[this.index],"on");
+			this.index = index;
+		}
+	},
+	setValue: function(This){
+		return function(){
+			This.obj.value = this.seq;
+			setClass.addClass(This.autoObj,"hidden");
+		}
+	},
+
+    pressKey: function(event){
+		var code = event.keyCode;
+		var length = this.autoObj.children.length;
+		if(code == 38){		//↑
+			setClass.removeClass(this.autoObj.children[this.index],"on");
+			this.index--;
+			if(this.index < 0){
+				this.index = length - 1;
+			}
+			setClass.addClass(this.autoObj.children[this.index],"on");
+			this.obj.value = this.autoObj.children[this.index].seq;
+		}else if(code == 40){	//↓
+			setClass.removeClass(this.autoObj.children[this.index],"on");
+			this.index++;
+			if(this.index > length-1){
+				this.index = 0;
+			}
+			setClass.addClass(this.autoObj.children[this.index],"on");
+			this.obj.value = this.autoObj.children[this.index].seq;
+		}else{			
+			this.obj.value = this.autoObj.children[this.index].seq;
+			setClass.addClass(this.autoObj,"hidden");
+			this.index = -1;
+		}
+	},
+
+    start: function(event){
+		event = event || window.event;
+		var code = event.keyCode;
+		var This = this;
+		if(code != 13 && code != 38 && code != 40){
+			this.init();
+			this.deleteDIV();
+			this.search_value = this.obj.value;
+			var valueArr = this.value_arr.unique();
+			if(this.obj.value.replace(/(^\s*)|(\s*$)/g,"") == ""){ return;}
+			try{
+				var reg = new RegExp("("+ this.obj.value +")","i");	
+			}catch(e){
+				alert(e.message); 
+			}
+			var div_index = 0;	
+			for (var i = 0; i < valueArr.length; i++) {
+				if(reg.test(valueArr[i])){
+					var div = document.createElement("div");
+					div.className = "auto_out";
+					div.seq = valueArr[i];
+					div.index = div_index;
+					div.innerHTML = valueArr[i].replace(reg,"<strong>$1</strong>");
+					this.autoObj.appendChild(div);
+					setClass.removeClass(this.autoObj,"hidden");
+					div_index++;
+					if(div_index == 1) {
+						setClass.addClass(this.autoObj.firstChild,"on");
+						this.index = 0;
+					}
+					div.onmouseover = function(){
+						This.autoOnmouseover(this.index);
+					}
+					div.onclick = this.setValue(This);
+				}
+			}
+		}else{
+			this.pressKey(event);
+		}
+		window.onresize = Bind(This);
+	}
+}
